0c498f0750f3d897e2d9295fa4de12fb2e265016
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-23 15:34:45 +0600

    revise let const

---
+++ ES6-Intro/js/let-const.js
@@ -0,0 +1,12 @@
+// let it to reassign
+// const -donot assign it to re assign
+let money =25;
+money = 35;
+console.log(money);
+
+const bird = 'jaan pakhi';
+// bird = 'moyna pakhi'; amon assign korle maan show korbe na
+console.log(bird);
+
+const massage = bird + 'khub shondor';
+console.log(massage);
